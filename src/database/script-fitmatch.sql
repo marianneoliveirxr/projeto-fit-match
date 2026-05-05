2434f75111f2f4da3e46880fc1092b53b25bff62
Marianne Oliveira <mariannerodrigues0806@gmail.com>
2026-05-04 22:13:09 -0300

    Iniciação da implementação da API web-data-viz

---
+++ src/database/script-fitmatch.sql
@@ -0,0 +1,52 @@
+create database fitMatch;
+use fitMatch;
+
+CREATE TABLE perfil(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    objetivo VARCHAR(45),
+    nivelFisico VARCHAR(45),
+    freq VARCHAR(45),
+    sexo VARCHAR(45),
+    CONSTRAINT chk_objetivo CHECK (objetivo IN ('emagrecer', 'hipertrofia')),
+    CONSTRAINT chk_nivelFisico CHECK (nivelFisico IN ('iniciante', 'intermediario', 'avancado')),
+	CONSTRAINT chk_freq CHECK (freq IN ('baixa', 'media', 'alta')),
+    CONSTRAINT chk_sexo CHECK (sexo IN ('feminino', 'masculino'))
+);
+
+create table usuario(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(80),
+    dtNasc DATE,
+    email VARCHAR(50),
+    senha VARCHAR(50),
+    confirmarSenha VARCHAR(50),
+    fkPerfil INT,
+    FOREIGN KEY (fkPerfil) REFERENCES perfil(id)
+);
+
+CREATE TABLE dadosCalculo(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    peso INT,
+    altura INT,
+    dtRegistro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fkUsuario INT,
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
+);	
+
+CREATE TABLE treino(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    divisao CHAR(1),
+    cardio VARCHAR(60),
+    fkPerfil INT,
+    CONSTRAINT chk_divisao CHECK (divisao IN('A', 'B', 'C')),
+    FOREIGN KEY (fkPerfil) REFERENCES perfil(id)
+);
+
+CREATE TABLE exercicios(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    exercicio VARCHAR(45),
+    series INT,
+	repeticoes INT,
+    fkTreino INT,
+    FOREIGN KEY (fkTreino) REFERENCES treino(id)
+);
