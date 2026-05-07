117e1add2f002f9fe5429b2b671367451125f63b
marianneoliveirxr <marianne.rodrigues@sptech.school>
2026-05-07 13:12:08 -0300

    Add: Implementação web-data-viz cadastro e login concluída

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -21,7 +21,6 @@ function autenticar(req, res) {
                         res.json({
                             id: resultadoAutenticar[0].id,
                             email: resultadoAutenticar[0].email,
-                            nome: resultadoAutenticar[0].nome,
                             senha: resultadoAutenticar[0].senha,
 
                         });
