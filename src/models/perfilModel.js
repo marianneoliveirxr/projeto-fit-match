1e38606ffa23fc5a15154c5ff9770e246bd52be8
marianneoliveirxr <marianne.rodrigues@sptech.school>
2026-05-10 21:34:18 -0300

    Add: validação tela plano

--- src/models/perfilModel.js
+++ src/models/perfilModel.js
@@ -3,8 +3,8 @@ var database = require("../database/config");
 function perfil(){
     console.log("ACESSEI O PERFIL MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
         var instrucaoSql = `
-                INSERT INTO perfil (objetivo, nivelFisico, freq, sexo) 
-                VALUES ('${objetivo}',' ${nivelFisico}', '${freq}', '${sexo}');
+                INSERT INTO perfil (objetivo, nivelFisico, sexo) 
+                VALUES ('${objetivo}',' ${nivelFisico}',  '${sexo}');
             `;
             console.log("Executando a instrução SQL: \n" + instrucaoSql);
             return database.executar(instrucaoSql);
