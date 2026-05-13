ea21e3d67fe7283d85b6f4d0842fe98568790a9d
marianneoliveirxr <marianne.rodrigues@sptech.school>
2026-05-12 21:29:55 -0300

    Add: css navbar dashboard

--- src/database/script-fitmatch.sql
+++ src/database/script-fitmatch.sql
@@ -1,31 +1,35 @@
 create database fitMatch;
 use fitMatch;
 
-create table usuario(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(80),
-    email VARCHAR(50),
-    senha VARCHAR(50)
-);
-
 CREATE TABLE perfil(
 	id INT PRIMARY KEY AUTO_INCREMENT,
     objetivo VARCHAR(45),
     nivelFisico VARCHAR(45),
     sexo VARCHAR(45),
-    fkUsuario INT,
+    peso INT,
+    altura INT,
     CONSTRAINT chk_objetivo CHECK (objetivo IN ('emagrecer', 'hipertrofia')),
     CONSTRAINT chk_nivelFisico CHECK (nivelFisico IN ('iniciante', 'intermediario', 'avancado')),
-    CONSTRAINT chk_sexo CHECK (sexo IN ('feminino', 'masculino')),
-    FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
+    CONSTRAINT chk_sexo CHECK (sexo IN ('feminino', 'masculino'))
 );
+
+create table usuario(
+	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(80),
+    email VARCHAR(50),
+    senha VARCHAR(50),
+    fkPerfil INT,
+    FOREIGN KEY (fkPerfil) REFERENCES perfil(id)
+);
+
+
 
 CREATE TABLE imc(
 	id INT PRIMARY KEY AUTO_INCREMENT,
     imc INT,
     dtRegistro DATETIME DEFAULT CURRENT_TIMESTAMP,
     fkUsuario INT,
-    FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(id_usuario)
 );	
 
 CREATE TABLE treino(
@@ -50,37 +54,17 @@ INSERT INTO perfil (objetivo, nivelFisico, sexo) VALUES
 ('emagrecer', 'iniciante', 'masculino'),
 ('emagrecer', 'intermediário', 'masculino'),
 ('emagrecer', 'avançado', 'masculino'),
-('emagrecer', 'iniciante', 'feminino'),
-('emagrecer', 'intermediário', 'feminino'),
-('emagrecer', 'avançado', 'feminino'),
 ('hipertrofia', 'iniciante', 'masculino'),
 ('hipertrofia', 'intermediário', 'masculino'),
 ('hipertrofia', 'avançado', 'masculino'),
+('emagrecer', 'iniciante', 'feminino'),
+('emagrecer', 'intermediário', 'feminino'),
+('emagrecer', 'avançado', 'feminino'),
 ('hipertrofia', 'iniciante', 'feminino'),
 ('hipertrofia', 'intermediário', 'feminino'),
 ('hipertrofia', 'avançado', 'feminino');
 
-desc treino;
-INSERT INTO treino (divisao, cardio, fkPerfil) VALUES
-('A', 'Escada ou Esteira', 1),
-('B', 'Escada ou Esteira', 2),
-('C', 'Escada ou Esteira', 3),
-('A', 'Escada ou Esteira', 4),
-('B', 'Escada ou Esteira', 5),
-('C', 'Escada ou Esteira', 6),
-('A', 'Esteira ou Bike', 7),
-('B', 'Esteira ou Bike', 8),
-('C', 'Esteira ou Bike', 9),
-('A', 'Esteira ou Bike', 10),
-('B', 'Esteira ou Bike', 11),
-('C', 'Esteira ou Bike', 12);
-
-
-INSERT INTO exercicios (exercicio, series, repeticoes, fkTreino) VALUES
-('Supino reto com halteres', 4, 8, 7),
-('Supino inclinado com halteres', 3, 10, 7),
-('Crucifixo', 3, 10, 7),
-('Desenvolvimento com halteres', 4, 10, 7),
-('Elevação Lateral', 3, 12, 7),
-('Tríceps Francês', 3, 10, 7),
-('Tríceps Corda', 3, 10, 7);
+SELECT * FROM usuario;
+UPDATE usuario 
+SET email = 'isabella@gmail.com'
+WHERE id_usuario = 1;
